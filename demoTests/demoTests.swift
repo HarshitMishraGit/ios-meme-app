cf6488b65ea9963b19ae3b6abeda2d94b78e45a3
Harshit Mishra <harshitlove28@gmail.com>
2025-06-19 12:19:16 +0530

    Initial Commit

---
+++ demoTests/demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  demoTests.swift
+//  demoTests
+//
+//  Created by Harshit Mishra on 19/06/25.
+//
+
+import Testing
+@testable import demo
+
+struct demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
